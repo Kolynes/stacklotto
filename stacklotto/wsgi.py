9d2809d44e2a8d8d4ddc238fb41174e6943607e6
Kolynes <kolyneschinedu@gmail.com>
2020-12-18 14:30:07 +0100

    updated settings configuration

--- stacklotto/wsgi.py
+++ stacklotto/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stacklotto.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stacklotto.settings.production")
 
 application = get_wsgi_application()
